1cdfd1eb441c91f2c292fdd78727ace7f0cd769d
nirre7 <niclas.soderstrom@gmp-systems.com>
2024-02-01 14:29:22 +0100

    Added groot endpoint

--- src/main/java/com/niso/poc/HelloWorld.java
+++ src/main/java/com/niso/poc/HelloWorld.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+    @GetMapping("/")
+    public String index() {
+        return "I'm groot!";
+    }
+
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello World!";
