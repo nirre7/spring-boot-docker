9fae6365c4492e162788fe25d3f19dbab43c1928
nirre7 <niclas.soderstrom@gmp-systems.com>
2024-02-02 11:27:59 +0100

    Removed actuator. Added shutdown

--- src/main/java/com/niso/poc/HelloWorld.java
+++ src/main/java/com/niso/poc/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.niso.poc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+
+    @Autowired
+    private ApplicationContext appContext;
 
     @GetMapping("/")
     public String index() {
@@ -14,5 +20,11 @@ public class HelloWorld {
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/api/shutdown")
+    public void shutdown() {
+        int exit = SpringApplication.exit(appContext, () -> 0);
+        System.exit(exit);
     }
 }
